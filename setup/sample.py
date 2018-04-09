8a773f3a8cd386a4f518c9b993aa31c8ec91bca2
kartik <chauhan.kartik25@gmail.com>
2018-04-10 00:06:48 +0530

    sample file for outputing console text on widget added

--- setup/sample.py
+++ setup/sample.py
@@ -1,19 +1,40 @@
-from tkinter import *
+import Tkinter as tk
+import sys
 
-root = Tk()
-root.title("sample program")
+class ExampleApp(tk.Tk):
+    def __init__(self):
+        tk.Tk.__init__(self)
+        toolbar = tk.Frame(self)
+        toolbar.pack(side="top", fill="x")
+        b1 = tk.Button(self, text="print to stdout", command=self.print_stdout)
+        b2 = tk.Button(self, text="print to stderr", command=self.print_stderr)
+        b1.pack(in_=toolbar, side="left")
+        b2.pack(in_=toolbar, side="left")
+        self.text = tk.Text(self, wrap="word")
+        self.text.pack(side="top", fill="both", expand=True)
+        self.text.tag_configure("stderr", foreground="#b22222")
 
-def print_item_from_list(event):
-    print(event.widget.config("text")[-1])
+        sys.stdout = TextRedirector(self.text, "stdout")
+        sys.stderr = TextRedirector(self.text, "stderr")
 
+    def print_stdout(self):
+        '''Illustrate that using 'print' writes to stdout'''
+        # print "this is stdout"
+        sys.stdout.write("printing")
 
-list = [1, 2, 3, 4, 5]
-seclist = []
-print(list)
-for i in range(0,5):
-    variable = list[i]
-    sample = Label(text=variable)
-    sample.pack()
-    sample.bind('<Enter>', print_item_from_list)
+    def print_stderr(self):
+        '''Illustrate that we can write directly to stderr'''
+        sys.stderr.write("this is stderr\n")
 
-root.mainloop()
+class TextRedirector(object):
+    def __init__(self, widget, tag="stdout"):
+        self.widget = widget
+        self.tag = tag
+
+    def write(self, str):
+        self.widget.configure(state="normal")
+        self.widget.insert("end", str, (self.tag,))
+        self.widget.configure(state="disabled")
+
+app = ExampleApp()
+app.mainloop()
